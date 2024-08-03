243761ee4bb8ebb1e6e2a6f2638191b50e222f33
Hridaya Patil <85823670+hridaya11@users.noreply.github.com>
2024-08-03 21:49:30 +0530

    add:new ui changes

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
-window.addEventListener('scroll', function() {
-    const header = document.querySelector('.header');
-    if (window.scrollY > 20) {
-      header.classList.add('glassy');
-    } else {
-      header.classList.remove('glassy');
-    }
-  });
-  
+window.addEventListener("scroll", function () {
+  const header = document.querySelector(".header");
+  if (window.scrollY > 20) {
+    header.classList.add("glassy");
+  } else {
+    header.classList.remove("glassy");
+  }
+});
+
+function toggleVisibility() {
+  document.querySelector("#burger").classList.toggle("hidden");
+  document.querySelector("#hdcross").classList.toggle("hidden");
+  document.querySelector(".nav").classList.toggle("show");
+}
+
+// Add event listener to the burger icon
+document.querySelector("#burger").addEventListener("click", () => {
+  toggleVisibility();
+});
+
+// Add event listener to the cross icon
+document.querySelector("#hdcross").addEventListener("click", () => {
+  toggleVisibility();
+});
